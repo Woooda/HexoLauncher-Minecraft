bb4c6b948cef38965150acbb4f719b9570890e96
Wooda <159943497+Woooda@users.noreply.github.com>
2024-04-10 19:20:47 +0300

    Update qt_version.py

--- qt_version.py
+++ qt_version.py
@@ -1,19 +1,12 @@
-from PyQt5.QtCore import QThread, pyqtSignal, QSize, Qt
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit, QComboBox, QSpacerItem, QSizePolicy, QProgressBar, QPushButton, QApplication, QMainWindow
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QListWidget, QSizePolicy, QProgressBar, QPushButton, QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
 
 from minecraft_launcher_lib.utils import get_minecraft_directory, get_version_list
 from minecraft_launcher_lib.install import install_minecraft_version
 from minecraft_launcher_lib.command import get_minecraft_command
 
-# Эти импорты не обязательны, вместо generate_username()[0] и str(uuid1()) можно оставить просто ''
-from random_username.generate import generate_username
-from uuid import uuid1
-
-from subprocess import call
-from sys import argv, exit
-
-minecraft_directory = get_minecraft_directory().replace('minecraft', 'mjnlauncher')
+minecraft_directory = get_minecraft_directory().replace('minecraft', 'hexolauncher')
 
 class LaunchThread(QThread):
     launch_setup_signal = pyqtSignal(str, str)
@@ -66,35 +59,44 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.resize(400, 300)
+        self.setWindowTitle("HexoLauncher")
+        self.setStyleSheet("color: white; background-color: #000F1A;")
+
         self.centralwidget = QWidget(self)
 
+        self.logo_path = "C:/Users/Eldreamer/Desktop/hexolauncher/assets/minecraft_logo.png"
         self.logo = QLabel(self.centralwidget)
         self.logo.setAlignment(Qt.AlignCenter)
-        self.logo.setPixmap(QPixmap('assets/title.png'))
-        self.logo.setScaledContents(True)
+        self.set_logo_icon(self.logo_path)
+
+        self.description = QLabel(self.centralwidget)
+        self.description.setText("Welcome! Choose your Minecraft version:")
+        self.description.setAlignment(Qt.AlignCenter)
+        self.description.setStyleSheet("font-size: 16px;")
+
+        self.version_list = QListWidget(self.centralwidget)
+        self.version_list.setStyleSheet("font-size: 14px; background-color: #34495E; border: 1px solid #2C3E50; border-radius: 5px; padding: 5px; color: white;")
+        self.populate_version_list()
 
         self.username = QLineEdit(self.centralwidget)
         self.username.setPlaceholderText('Enter Username')
         self.username.setAlignment(Qt.AlignCenter)
-
-        self.version_select = QComboBox(self.centralwidget)
-        for version in get_version_list():
-            self.version_select.addItem(version['id'])
-        self.version_select.setStyleSheet("QComboBox { color: black; background-color: #e6e6e6; border: 1px solid #cccccc; border-radius: 5px; padding: 5px; }")
+        self.username.setStyleSheet("font-size: 14px; background-color: #34495E; border: 1px solid #2C3E50; border-radius: 5px; padding: 5px; color: white;")
 
         self.start_progress = QProgressBar(self.centralwidget)
         self.start_progress.setVisible(False)
+        self.start_progress.setStyleSheet("QProgressBar { color: #3498DB; border: 1px solid #2C3E50; border-radius: 5px; background-color: #34495E; } QProgressBar::chunk { background-color: #2E86C1; }")
 
         self.start_button = QPushButton(self.centralwidget)
         self.start_button.setText('Play')
-        self.start_button.setStyleSheet("QPushButton { background-color: #4CAF50; color: white; border: none; border-radius: 5px; padding: 10px; font-size: 16px; } QPushButton:hover { background-color: #45a049; }")
+        self.start_button.setStyleSheet("QPushButton { background-color: #3498DB; color: white; border: none; border-radius: 5px; padding: 10px; font-size: 16px; } QPushButton:hover { background-color: #2E86C1; }")
         self.start_button.clicked.connect(self.launch_game)
 
         self.vertical_layout = QVBoxLayout(self.centralwidget)
         self.vertical_layout.addWidget(self.logo)
+        self.vertical_layout.addWidget(self.description)
+        self.vertical_layout.addWidget(self.version_list)
         self.vertical_layout.addWidget(self.username)
-        self.vertical_layout.addWidget(self.version_select)
         self.vertical_layout.addWidget(self.start_progress)
         self.vertical_layout.addWidget(self.start_button)
         self.vertical_layout.setAlignment(Qt.AlignCenter)
@@ -104,23 +106,33 @@ class MainWindow(QMainWindow):
         self.launch_thread.progress_update_signal.connect(self.update_progress)
 
         self.setCentralWidget(self.centralwidget)
+
+    def set_logo_icon(self, path):
+        pixmap = QPixmap(path)
+        self.logo.setPixmap(pixmap.scaledToHeight(200))  # Установите размеры изображения
+
+    def populate_version_list(self):
+        versions = get_version_list()
+        for version in versions:
+            self.version_list.addItem(version['id'])
+        
     def state_update(self, value):
         self.start_button.setDisabled(value)
-        self.start_progress_label.setVisible(value)
         self.start_progress.setVisible(value)
+
     def update_progress(self, progress, max_progress, label):
         self.start_progress.setValue(progress)
         self.start_progress.setMaximum(max_progress)
-        self.start_progress_label.setText(label) # Исправил проблему с созданием описания для полосы прогресса [24:01]
+        self.start_progress.setFormat(label)
+
     def launch_game(self):
-        self.launch_thread.launch_setup_signal.emit(self.version_select.currentText(), self.username.text())
+        selected_version = self.version_list.currentItem().text()
+        self.launch_thread.launch_setup_signal.emit(selected_version, self.username.text())
         self.launch_thread.start()
 
 if __name__ == '__main__':
-    QApplication.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling, True)
-
-    app = QApplication(argv)
+    app = QApplication([])
     window = MainWindow()
     window.show()
+    app.exec_()
 
-    exit(app.exec_())
